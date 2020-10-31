93c7741fb2ab3046b8c1338450b968cf82a61dc2
Alla Rudenko <whitney2206@gmail.com>
2020-10-31 11:57:07 -0400

    change is here

--- src/test/java/Halloween.java
+++ src/test/java/Halloween.java
@@ -3,5 +3,7 @@ public class Halloween {
     public static void main(String[] args) {
 
         System.out.println("Hello Halloween");
+
+        System.out.println("I'm about to give up here");
     }
 }
