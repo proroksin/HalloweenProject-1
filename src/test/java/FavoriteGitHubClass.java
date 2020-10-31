0ac4d525f3f7b05d090f5cd78f59e904749d4c70
Alla Rudenko <whitney2206@gmail.com>
2020-10-31 13:55:21 -0400

    Love my Saturday's classes

--- src/test/java/FavoriteGitHubClass.java
+++ src/test/java/FavoriteGitHubClass.java
@@ -3,5 +3,7 @@ public class FavoriteGitHubClass {
     public static void main(String[] args) {
 
         System.out.println("Saturday is the best day of the week");
+        System.out.println("Saturday is the best day of the week");
+
     }
 }
